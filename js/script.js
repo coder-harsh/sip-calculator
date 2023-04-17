d6f8eade8bbbfcc16906788c85d1e933974e02f6
Harsh <harshj7.net@gmail.com>
2023-04-18 02:37:56 +0530

    trunc the decimal value in calculator

--- js/script.js
+++ js/script.js
@@ -66,14 +66,14 @@ setInterval(() => {
 
 // comp_interest_1
 setInterval(() => {
-    comp_interest_1.innerText = principl_1_inp.value * (1 + 6 / 100) ** time_inp.value;
+    comp_interest_1.innerText = Math.trunc(principl_1_inp.value * (1 + 6 / 100) ** time_inp.value);
 })
 // comp_interest_2
 setInterval(() => {
-    comp_interest_2.innerText = principl_2_inp.value * (1 + risk_inp.value / 100) ** time_inp.value;
+    comp_interest_2.innerText = Math.trunc(principl_2_inp.value * (1 + risk_inp.value / 100) ** time_inp.value);
 })
 
 // sol_ci_diff
 setInterval(() => {
-    sol_ci_diff.innerText = (principl_1_inp.value * (1 + 6 / 100) ** time_inp.value) - (principl_2_inp.value * (1 + risk_inp.value / 100) ** time_inp.value);
+    sol_ci_diff.innerText = Math.trunc((principl_1_inp.value * (1 + 6 / 100) ** time_inp.value) - (principl_2_inp.value * (1 + risk_inp.value / 100) ** time_inp.value));
 })
